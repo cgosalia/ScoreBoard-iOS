dba1d4f9f37f1ebcc0c2de41d5b6f6993ce19251
cgravivarsha <cgravivarsha@gmail.com>
2014-04-14 00:13:46 -0400

    until image transfer and new message format

--- BentoMobilityScoreBoardAppNewWithUpdateButton/Bento Mobility Score Tracking App/AppDelegate.m
+++ BentoMobilityScoreBoardAppNewWithUpdateButton/Bento Mobility Score Tracking App/AppDelegate.m
@@ -23,11 +23,7 @@ bool didEnterBackground=false;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    
-    NSLog(@"Entered Background");
-    didEnterBackground=true;
-    
+{    
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
@@ -40,9 +36,7 @@ bool didEnterBackground=false;
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"Lost Connection" message:nil delegate:nil cancelButtonTitle:nil otherButtonTitles:nil];
-    if(didEnterBackground)
-        NSLog(@"Became Active");
+
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
